1bf177c4484a3dd01bbd032b8e243ba98eb8c058
alphen11330 <alphen11330@gmail.com>
2026-03-03 23:55:06 +0900

    Revert "feat: AWS serverless architecture integration"
    This reverts commit 276fd8df86cd5ddc37ea7a1b8b8d72798cbb3e3b.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -259,7 +259,6 @@ export default function Page() {
                   dotsImageSrc={dotsImageSrc}
                   isRecommendedSize={isRecommendedSize}
                   isJP={isJP}
-                  colorPalette={colorPalette}
                 />
                 {/* リフレッシュボタン */}
                 <RefreshButton
